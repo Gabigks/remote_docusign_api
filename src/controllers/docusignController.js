4ed2795d7938c769284cf4bb5485843f8788d5fc
Kolling <gabrielgks10@gmail.com>
2024-01-02 10:43:47 -0300

    hard code to test more than 1 'sign here' tab

--- src/controllers/docusignController.js
+++ src/controllers/docusignController.js
@@ -12,22 +12,26 @@ export const postEnvelope = async (request, response) => {
   const template = await templatesApi.createTemplate(process.env.ACCOUNT_ID, { envelopeTemplate: templateData });
   const templateId = template.templateId
 
-  const documentData = await templateDocument();
+  const documentData = await templateDocument(request);
   const documentId = '1';
   await templatesApi.updateDocument(process.env.ACCOUNT_ID, templateId, documentId, { envelopeDefinition: documentData });
 
-  const tabs = recipientTabs();
-  const recipientId = '1';
+  let tabs = recipientTabs(request.body.anchorString);
+  let recipientId = '1';
+  await templatesApi.createTabs(process.env.ACCOUNT_ID, templateId, recipientId, { templateTabs: tabs });
+  
+  tabs = recipientTabs(request.body.anchorString2);
+  recipientId = '2';
   await templatesApi.createTabs(process.env.ACCOUNT_ID, templateId, recipientId, { templateTabs: tabs });
 
-  const envelopeData = makeEnvelope(request.body.name, request.body.email, templateId);
+  const envelopeData = makeEnvelope(request, templateId);
   const envelope = await envelopesApi.createEnvelope(process.env.ACCOUNT_ID, { envelopeDefinition: envelopeData });
   const envelopeId = envelope.envelopeId;
 
   const docGenFormFieldsResponse = await envelopesApi.getEnvelopeDocGenFormFields(process.env.ACCOUNT_ID, envelopeId);
   const documentIdGuid = docGenFormFieldsResponse.docGenFormFields[0].documentId;
 
-  const formFieldsData = formFields(documentIdGuid, request.body.name, request.body.email, request.body.company);
+  const formFieldsData = formFields(documentIdGuid, request);
   await envelopesApi.updateEnvelopeDocGenFormFields(process.env.ACCOUNT_ID, envelopeId, { docGenFormFieldRequest: formFieldsData });
 
   // response.redirect(results.url);
